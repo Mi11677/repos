6250cfa8fecf95b772652b051d5d76f34842e4ef
Misha <oxot1nik@yandex.ru>
2023-08-24 19:32:03 +0300

    Init

---
+++ Java/Main.java
@@ -0,0 +1,43 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t\t\t\t\t\t\t\t В лесу родилась Ёлочка \"Антоним.\"");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t ");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t В пустыне помирал дубок");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Дубок в пустыне чах");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Весной и в осень кривенький");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t В коричневых тонах");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t ");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Самум ему стихи читал");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t ПодЪём дубок, вставай");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t А зной песок раскапывал");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Давай позагарай!");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t ");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Отважный лев,зелёный лев");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Над дубом пролетал");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Частенько радостный верблюд ");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Галопом проползал");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t ");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Пустынею разреженной");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t На стыках рельс стучит");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Креветка сиволапая");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Неспешно тормозит");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t ");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Она телегу не несёт");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t И бабы нету в ней");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t Что склеит прочно дуб чужой");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t До верхних до ветвей");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t ");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t И вот он безобразненький");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t С поминок убежал");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t И мало, мало гадостей");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t У старичков отнял");
+    }
+}
